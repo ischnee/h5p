d39e3f6f4db30cd78f91709b56b7a4f6d606c49f
ischnee <42156993+ischnee@users.noreply.github.com>
2020-10-23 13:03:56 -0700

    Update show-solution-button-after-n-attempts.js

--- show-solution-button-after-n-attempts.js
+++ show-solution-button-after-n-attempts.js
@@ -1,29 +1,27 @@
-<script>
-    const iframe = document.getElementById("h5p-iframe-717");
-    let tryNumber = 1;
-    console.log(tryNumber);
+const iframe = document.getElementById("h5p-iframe-717");
+let tryNumber = 1;
+console.log(tryNumber);
     
-	document.getElementById("h5p-iframe-717").onload = function() {
-		let byClass = iframe.contentWindow.document.getElementsByClassName('h5p-question-check-answer');
-		var checkAnsButton = byClass[0];
-		console.log(byClass);
+document.getElementById("h5p-iframe-717").onload = function() {
+	let byClass = iframe.contentWindow.document.getElementsByClassName('h5p-question-check-answer');
+	var checkAnsButton = byClass[0];
+	console.log(byClass);
 
-		function tryAdd() {
+	function tryAdd() {
       		tryNumber++;
-			console.log(tryNumber);
-			var solButtonClass = iframe.contentWindow.document.getElementsByClassName('h5p-question-show-solution');
+		console.log(tryNumber);
+		var solButtonClass = iframe.contentWindow.document.getElementsByClassName('h5p-question-show-solution');
             
-			if (tryNumber < 3){
-				solButtonClass[0].setAttribute('title', 'Try again before viewing solution');
-				solButtonClass[0].setAttribute('style', 'background: #b2c6dd');
-				solButtonClass[0].disabled = true;
-			} else {
-				solButtonClass[0].disabled = false;
-				solButtonClass[0].setAttribute('title', 'Show solution');
-				solButtonClass[0].setAttribute('style', '');
-			}
+		if (tryNumber < 3){
+			solButtonClass[0].setAttribute('title', 'Try again before viewing solution');
+			solButtonClass[0].setAttribute('style', 'background: #b2c6dd');
+			solButtonClass[0].disabled = true;
+		} else {
+			solButtonClass[0].disabled = false;
+			solButtonClass[0].setAttribute('title', 'Show solution');
+			solButtonClass[0].setAttribute('style', '');
 		}
+  	}
 
-		checkAnsButton.addEventListener('click', tryAdd, false);
-	}
-</script>
+	checkAnsButton.addEventListener('click', tryAdd, false);
+}
